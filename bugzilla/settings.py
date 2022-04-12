8cb53c6fa4667a089993f6af5e50669756f06516
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-04-12 12:21:31 +0500

    Debug is False

--- bugzilla/settings.py
+++ bugzilla/settings.py
@@ -140,12 +140,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
 
 # TODO: commment for deployment
-# EMAIL_USE_TLS = env('EMAIL_USE_TLS')
-# EMAIL_HOST = env('EMAIL_HOST')
-# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = env('EMAIL_PORT')
-
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
 
 
 import django_heroku

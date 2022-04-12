527424fda54c006d27d9d75f0de67a06e37953db
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-04-12 12:19:37 +0500

    Debug is True

--- bugzilla/settings.py
+++ bugzilla/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = False TODO: Uncomment for Deployment
 
 ALLOWED_HOSTS = ['*']
@@ -146,9 +146,6 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # EMAIL_PORT = env('EMAIL_PORT')
 
-# TODO: uncommment for deployment
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 
 
 import django_heroku

295189551a5076396a850dbfac456d126d290edb
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-04-22 15:59:05 +0500

    Add Fail cases to Project Model Test

--- project/models/project_model.py
+++ project/models/project_model.py
@@ -8,7 +8,6 @@ class Project(models.Model):
     )
 
     description = models.TextField(
-        max_length=250,
         verbose_name="Project Description",
     )
 
--- project/tests/models_tests/test_project_model.py
+++ project/tests/models_tests/test_project_model.py
@@ -1,28 +1,37 @@
 from django.test import TestCase
 
 from project.tests.factories import ProjectFactory
+from faker import Factory
+from django.db.utils import DataError
 
+faker = Factory.create()
 
 class ProjectModelTest(TestCase):
     def setUp(self):
         self.project = ProjectFactory()
 
-    def test_profile_object_name(self):
+    def test_happy_cases(self):
+
+        # test_profile_object_name
         expected_object_name = f"{self.project.name}"
         self.assertEqual(str(self.project), expected_object_name)
 
-    def test_project_name_length(self):
+        #test_project_name_length
         project_name_length = self.project._meta.get_field("name").max_length
         self.assertEqual(project_name_length, 50)
 
-    def test_project_description_length(self):
-        project_name_length = self.project._meta.get_field("description").max_length
-        self.assertEqual(project_name_length, 250)
-
-    def test_project_name_verbose(self):
+        #test_project_name_verbose
         project_name = self.project._meta.get_field("name").verbose_name
         self.assertEqual(project_name, "Project Name")
 
-    def test_project_description_verbose(self):
+        #test_project_description_verbose
         project_description = self.project._meta.get_field("description").verbose_name
         self.assertEqual(project_description, "Project Description")
+
+    def test_exceeding_length_project_name(self):
+        try:
+            self.project.name = faker.sentence(51)
+            self.project.save()
+            self.fail("Error not raised at project_name max_length")
+        except DataError as e:
+            pass

19a2ca3d35316c55da556a6f59c53977bf7791f6
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-04-22 15:46:06 +0500

    Add Fail cases to UserProfile Model Test

--- userprofile/tests/models_tests/user_profile_model_test.py
+++ userprofile/tests/models_tests/user_profile_model_test.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
 
 from django.contrib.auth.models import User
 from userprofile.models import Profile
@@ -7,7 +9,12 @@ from project.tests.factories.project import ProjectFactory
 from userprofile.tests.factories.profile import ProfileFactory
 from userprofile.tests.factories.user import UserFactory
 
+from faker import Factory
+
 from utilities import USER_TYPES, MANAGER
+
+
+faker = Factory.create()
 
 
 class UserProfileTest(TestCase):
@@ -15,34 +22,54 @@ class UserProfileTest(TestCase):
     def setUpTestData(cls):
         user = UserFactory()
         profile = ProfileFactory(user=user, project=None)
-        # print(f"User Details: usernamae: {user.username}, email: {user.email}, profile designation: {profile.designation}")
 
     def setUp(self):
         self.user = User.objects.last()
         self.profile = Profile.objects.last()
 
-    def test_only_single_profile_linked_with_user(self):
+    def test_happy_cases(self):
+        # Profiles linked with single user
         profile_count = Profile.objects.filter(user=self.user).count()
         self.assertEqual(profile_count, 1)
-
-    def test_designation_choices(self):
+        # Choices allowed for Profile Types (designation)
         choices = self.profile._meta.get_field("designation").choices
         self.assertEqual(
             choices,
             USER_TYPES + ((MANAGER, "Manager"),),
         )
-
-    def test_project_foreign_key_name(self):
+        # Adding Project to profile and verifying its added
         project = ProjectFactory()
         project.save()
         self.profile.project = project
         self.profile.save()
-        # print(f"\nProject Added: Name: {self.profile.project.name}\nDescription: {self.profile.project.description}\n")
         self.assertEqual(self.profile.project.name, project.name)
-
-    def test_profile_object_name(self):
+        # Checking if object name is same as set by str method
         expected_object_name = f"{self.profile.user.username}"
         self.assertEqual(str(self.profile), expected_object_name)
+        # Verifying url the is got reverse operation is correct
+        self.assertEqual(
+            self.profile.get_absolute_url(), f"/users/{self.profile.user.id}"
+        )
 
-    def test_get_absolute_url(self):
-        self.assertEqual(self.profile.get_absolute_url(), f"/users/{self.profile.user.id}")
+    def test_no_user_profile(self):
+        try:
+            ProfileFactory(user=None, project=None)
+            self.fail("Profile existing without user")
+        except IntegrityError as e:
+            pass
+
+    def test_link_mutliple_profiles(self):
+        try:
+            ProfileFactory(user=self.user, project=None, designation=MANAGER)
+            self.fail("Duplicate profile created for same user")
+        except Exception as e:
+            self.assertEqual(Profile.objects.all().count(), User.objects.all().count())
+
+    def test_custom_designation_for_user(self):
+        try:
+            self.profile.designation = faker.word(['random', 'words', 'for', 'design', 'ation'])
+            self.profile.full_clean()
+            self.profile.save()
+            self.fail("Random Designation Assigned")
+        except ValidationError as e:
+            pass

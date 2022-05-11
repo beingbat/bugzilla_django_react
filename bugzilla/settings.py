9d410ba407b0faa7c817aed18785e9968babddea
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-05-11 14:03:07 +0500

    Add corsheaders and update gitignore

--- bugzilla/settings.py
+++ bugzilla/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'corsheaders',
     "cloudinary",
     "userprofile",
     "project",
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -166,6 +168,14 @@ cloudinary.config(
     api_secret=env("CLOUDINARY_SECRET"),
 )
 
+
 import django_heroku
 
 django_heroku.settings(locals())
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    # "*",
+
+]

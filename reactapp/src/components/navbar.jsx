0934777d98ec049917db4d069e91c04430987656
Arsalan Malek <arsalan.tahir@devsinc.com>
2022-05-16 00:08:54 +0500

    Fix Navbar styling

--- reactapp/src/components/navbar.jsx
+++ reactapp/src/components/navbar.jsx
@@ -1,29 +1,30 @@
 import React from "react";
+import "./navbar.css";
 import { Container, Row, Col, Navbar, NavbarBrand, Nav, NavLink } from "reactstrap";
 
 const Navigation = () => {
   return (
-    <Navbar bg="dark" variant="dark" sticky='top' >
-      <NavbarBrand className="mx-3" href="#">
-        <Container>
-          <Row>
+    <Navbar className="color-nav" sticky='top' >
+      <NavbarBrand className="mx-3 pt-2" href="#">
+        <Container className="">
+          <Row className="">
             <Col>
-              <p className="display-7 mr_auto">
+              <p className="display-7 white">
                 React
                 <br />
                 App
               </p>
             </Col>
-            <Col className="mr_auto">
-              <p className="display-6 mr_auto">BUGZILLA</p>
+            <Col className="">
+              <p className="display-6 mt-2 white">BUGZILLA</p>
             </Col>
           </Row>
         </Container>
       </NavbarBrand>
 
-      <Nav>
-        <NavLink href="/">DASHBOARD</NavLink>
-        <NavLink href="/logout">LOGOUT</NavLink>
+      <Nav className="pb-2 mr-auto">
+        <NavLink className="white" href="/">DASHBOARD</NavLink>
+        <NavLink className="white" href="/accounts/logout">LOGOUT</NavLink>
       </Nav>
     </Navbar>
   );
